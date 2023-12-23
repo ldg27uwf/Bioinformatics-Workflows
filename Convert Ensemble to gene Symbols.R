147549d1d26d5e7eb9583b96e154602ff813ac9c
Lindsay Guyette <134321700+ldg27uwf@users.noreply.github.com>
2023-12-23 00:03:16 -0600

    Update Convert Ensemble to gene Symbols.R
    completed ensemble to gene id tutorial

--- Convert Ensemble to gene Symbols.R
+++ Convert Ensemble to gene Symbols.R
@@ -10,7 +10,7 @@ BiocManager::install("EnsDb.Hsapiens.v86", force = TRUE)
 # annotables not compatible with this version of R
 
 install.packages("devtools")
-devtools::install_github("stephenturner/annotables")
+devtools::install_github("stephenturner/annotables", force = TRUE)
 library(biomaRt)
 library(org.Hs.eg.db)
 library(EnsDb.Hsapiens.v86)
@@ -19,10 +19,47 @@ library(annotables)
 library(tidyverse)
 
 # input list of Ensembl ID's
-GENE_ID_LIST <- read.table("~/Desktop/Bioinformagician/Bioinformatics_Lessons/GENE_ID_LIST", quote="\"", comment.char="")
+ensembl.ids <- read.table("~/Desktop/Bioinformagician/Bioinformatics_Lessons/GENE_ID_LIST", quote="\"", comment.char="", header = F)
 View(GENE_ID_LIST)
 
 # method 1: BiomaRt
 listEnsembl()
 ensembl <- useEnsembl(biomart = "genes")
 datasets <- listDatasets(ensembl)
+view(datasets)
+
+ensembl.con <- useMart("ensembl", dataset = 'hsapiens_gene_ensembl')
+
+attr <- listAttributes(ensembl.con)
+filters <- listFilters(ensembl.con)
+
+# query for gene ID and gene name
+getBM(attributes = c('ensembl_gene_id', 'external_gene_name'),
+      filters = "ensembl_gene_id",
+      values = ensembl.ids$V1,
+      mart = ensembl.con
+      )
+
+# method 2: annotables
+
+grch38 %>%
+  filter(ensgene %in% ensembl.ids$V1)
+
+# method 3: annotation DBs
+
+keytypes(org.Hs.eg.db)
+columns(org.Hs.eg.db)
+
+mapIds(org.Hs.eg.db,
+       keys = ensembl.ids$V1,
+       keytype = 'ENSEMBL',
+       column = 'SYMBOL')
+
+keytypes(EnsDb.Hsapiens.v86)
+
+columns(EnsDb.Hsapiens.v86)
+
+mapIds(EnsDb.Hsapiens.v86,
+       keys = ensembl.ids$V1,
+       keytype = 'GENEID',
+       column = 'SYMBOL')
